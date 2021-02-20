f3b96156d80a9e1b3bdab979d2e8845ff4648e68
Bestbrain Igbaji Livinus <bestbrain10@gmail.com>
2021-02-20 15:51:09 +0100

    add eslint, fix lint errors

--- resolvers/message.js
+++ resolvers/message.js
@@ -3,37 +3,37 @@ const users = require('../models/user');
 const _ = require('lodash');
 
 module.exports = { Query: {
-    messages: (parent, args) => {
-      return messages;
-    },
-    message: (parent, { id }) => {
-      return messages.find(({ id: messageID }) => messageID == id);
-    },
+	messages: () => {
+		return messages;
+	},
+	message: (parent, { id }) => {
+		return messages.find(({ id: messageID }) => messageID == id);
+	},
 },
-  Mutation: {
-    createMessage: (parent, { text }, { me }) => {
-      const { id: lastID } = _.last(messages);
-      const id = lastID+1;
-      const message = {
-        id,
-        text,
-        user_id: me.id,
-      };
-      messages[id] = message;
-      return message;
-    },
-    deleteMessage: (parent, { id }) => {
-        const messageExists = !!messages.find(({ id: messageID }) => id == messageID);
-        if (!messageExists) {
-            return false;
-        }
-        messages = messages.filter(({ id: messageID }) => messageID != id);
-        return true;
-    },
-  },
-  Message: {
-    user: (message, args, { models }) => {
-        return users.find(({id}) => id == message.user_id);
-    },
-  }, 
+Mutation: {
+	createMessage: (parent, { text }, { me }) => {
+		const { id: lastID } = _.last(messages);
+		const id = lastID+1;
+		const message = {
+			id,
+			text,
+			user_id: me.id,
+		};
+		messages[id] = message;
+		return message;
+	},
+	deleteMessage: (parent, { id }) => {
+		const messageExists = !!messages.find(({ id: messageID }) => id == messageID);
+		if (!messageExists) {
+			return false;
+		}
+		messages = messages.filter(({ id: messageID }) => messageID != id);
+		return true;
+	},
+},
+Message: {
+	user: (message) => {
+		return users.find(({id}) => id == message.user_id);
+	},
+}, 
 };
