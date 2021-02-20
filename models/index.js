b9d7fc88b256f777fc7e326efdef653ab2b7c006
Bestbrain Igbaji Livinus <bestbrain10@gmail.com>
2021-02-20 16:01:13 +0100

    add postgres models and .env with db config

---
+++ models/index.js
@@ -0,0 +1,25 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize( 
+	process.env.DATABASE, 
+	process.env.DATABASE_USER, 
+	process.env.DATABASE_PASSWORD, 
+	{
+		dialect: 'postgres',
+	});
+
+const models = {
+	User: sequelize.import('./user'),
+	Message: sequelize.import('./message'),
+};
+Object.keys(models).forEach(key => {
+	if ('associate' in models[key]) {
+		models[key].associate(models);
+	}
+});
+
+
+module.exports = {
+	sequelize,
+	models
+};
